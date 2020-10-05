59fb0d4f09fec0871c245264b87aabfe73d31842
thisisaaronland <devnull@localhost>
2020-10-05 11:28:46 -0700

    start working to support aaronland/go-aws-session credentials

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/sfomuseum/runtimevar
 
 go 1.12
 
-require gocloud.dev v0.20.0
+require (
+	github.com/aaronland/go-aws-session v0.0.4
+	gocloud.dev v0.20.0
+)
--- string.go
+++ string.go
@@ -2,7 +2,11 @@ package runtimevar
 
 import (
 	"context"
+	"fmt"
 	gc "gocloud.dev/runtimevar"
+	"gocloud.dev/runtimevar/awsparamstore"
+	"github.com/aaronland/go-aws-session"
+	"log"
 	"net/url"
 )
 
@@ -21,12 +25,43 @@ func StringVar(ctx context.Context, uri string) (string, error) {
 		u.RawQuery = q.Encode()
 	}
 
-	uri = u.String()
+	var v *gc.Variable
+	var v_err error
+	
+	switch u.Scheme {
+	case "awsparamstore":
 
-	v, err := gc.OpenVariable(ctx, uri)
+		// https://gocloud.dev/howto/runtimevar/#awsps-ctor
+		
+		creds := q.Get("credentials")
+		region := q.Get("region")
+		
+		if creds != "" {
 
-	if err != nil {
-		return "", err
+			dsn_str := fmt.Sprintf("region=%s credentials=%s", region, creds)
+			log.Println("CREDS", dsn_str)
+			sess, err := session.NewSessionWithDSN(dsn_str)
+
+			if err != nil {
+				return "", err
+			}
+
+			v, v_err = awsparamstore.OpenVariable(sess, u.Path, gc.StringDecoder, nil)
+		}
+		
+	default:
+		// pass
+	}
+
+	if v == nil {
+		
+		uri = u.String()
+		
+		v, v_err = gc.OpenVariable(ctx, uri)
+	}
+	
+	if v_err != nil {
+		return "", v_err
 	}
 
 	defer v.Close()
