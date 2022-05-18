26d6448cbf4dc47c0f378ca7529712dfb573237e
thisisaaronland <aaron@localhost>
2022-05-18 11:07:42 -0700

    improved docs; tests

---
+++ string_test.go
@@ -0,0 +1,64 @@
+package runtimevar
+
+import (
+	"context"
+	"fmt"
+	_ "gocloud.dev/runtimevar/constantvar"
+	_ "gocloud.dev/runtimevar/filevar"
+	"path/filepath"
+	"testing"
+)
+
+func TestConstantVar(t *testing.T) {
+
+	ctx := context.Background()
+
+	tests := map[string]string{
+		"constant://?val=hello+world": "hello world",
+	}
+
+	for uri, expected := range tests {
+
+		str_var, err := StringVar(ctx, uri)
+
+		if err != nil {
+			t.Fatalf("Failed to retrieve string var for '%s', %v", uri, err)
+		}
+
+		if str_var != expected {
+			t.Fatalf("Unexpected result for '%s'. Expected '%s' but got '%s'", uri, expected, str_var)
+		}
+	}
+
+}
+
+func TestFileVar(t *testing.T) {
+
+	ctx := context.Background()
+
+	tests := map[string]string{
+		"fixtures/helloworld.txt": "hello world\n",
+	}
+
+	for rel_path, expected := range tests {
+
+		abs_path, err := filepath.Abs(rel_path)
+
+		if err != nil {
+			t.Fatalf("Failed to determine absolute path for '%s', %v", rel_path, err)
+		}
+
+		uri := fmt.Sprintf("file://%s?decoder=string", abs_path)
+
+		str_var, err := StringVar(ctx, uri)
+
+		if err != nil {
+			t.Fatalf("Failed to retrieve string var for '%s', %v", uri, err)
+		}
+
+		if str_var != expected {
+			t.Fatalf("Unexpected result for '%s'. Expected '%s' but got '%s'", uri, expected, str_var)
+		}
+	}
+
+}
