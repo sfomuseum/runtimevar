1705f903f58aea9741e822eb4ee7f5d69f73e7d4
sfomuseumbot <sfomuseumbot@localhost>
2024-02-06 15:53:56 -0800

    snapshot: wire in use ssm stuff; not sure it works

--- string.go
+++ string.go
@@ -3,7 +3,8 @@ package runtimevar
 import (
 	"context"
 	"fmt"
-	"github.com/aaronland/go-aws-session"
+	// "github.com/aaronland/go-aws-session"
+	"github.com/aaronland/go-aws-auth"	
 	gc "gocloud.dev/runtimevar"
 	"gocloud.dev/runtimevar/awsparamstore"
 	_ "gocloud.dev/runtimevar/constantvar"
@@ -42,6 +43,7 @@ func StringVar(ctx context.Context, uri string) (string, error) {
 
 		if creds != "" {
 
+			/*
 			dsn_str := fmt.Sprintf("region=%s credentials=%s", region, creds)
 			sess, err := session.NewSessionWithDSN(dsn_str)
 
@@ -50,6 +52,16 @@ func StringVar(ctx context.Context, uri string) (string, error) {
 			}
 
 			v, v_err = awsparamstore.OpenVariable(sess, u.Host, gc.StringDecoder, nil)
+			*/
+
+			aws_uri := fmt.Sprintf("aws://%s?credentials=%s", region, creds)
+			aws_auth, err := auth.NewSSMClient(ctx, aws_uri)
+
+			if err != nil {
+				return "", fmt.Errorf("Failed to create AWS session credentials, %w", err)
+			}
+
+			v, v_err = awsparamstore.OpenVariableV2(aws_auth, u.Host, gc.StringDecoder, nil)			
 		}
 
 	default:
